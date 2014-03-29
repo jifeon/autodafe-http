95c4f357ebc2d61583e78b5a3c63f35770354979
jifeon <balakirev.andrey@gmail.com>
2014-03-30 00:07:52 +0400

    working with params of requests

--- tests/apps/http_app/index.js
+++ tests/apps/http_app/index.js
@@ -2,7 +2,7 @@ var autodafe = require('autodafe');
 
 module.exports = {
     getApp: function (httpConfig) {
-        return autodafe.config({
+        var app = autodafe.config({
             basePath: __dirname,
             silent: true,
 
@@ -10,5 +10,19 @@ module.exports = {
                 http: httpConfig
             }
         });
+
+        var component = new autodafe.Component({
+            name: 'test'
+        });
+
+        component.processRequest = function (request, callback) {
+            if (request.getPath() == '/action') {
+                var params = request.getParams();
+                request.end(params['param1'] + ',' + params['param2']);
+            }
+            callback();
+        };
+
+        app.load(component);
     }
 };
--- tests/vows/http_server_tests.js
+++ tests/vows/http_server_tests.js
@@ -5,6 +5,71 @@ var vows = require('vows'),
     simpleAppPort = tools.getFreePort(),
     basicAuthAppPort = tools.getFreePort();
 
+function sendRequest(method, path) {
+    return function () {
+        var self = this;
+        http.request({
+            port: simpleAppPort,
+            path: path,
+            method: method.toUpperCase()
+        }, function (response) {
+            self.callback(null, response);
+        }).on('error', function (e) {
+            self.callback(e);
+        }).end();
+    };
+}
+
+function queryFileTest(method) {
+    return {
+        topic: sendRequest(method, '/test.html'),
+        'with status 200': function (e, response) {
+            assert.isNull(e);
+            assert.equal(response.statusCode, 200);
+        },
+        'with content': {
+            topic: function (response) {
+                var self = this;
+                response
+                    .on('data', function (data) {
+                        self.callback(null, data.toString());
+                    })
+                    .on('error', function (e) {
+                        self.callback(e);
+                    })
+            },
+            'equals to Hello World': function (e, data) {
+                assert.equal(data, 'Hello World');
+            }
+        }
+    }
+}
+
+function queryWithParams(method) {
+    return {
+        topic: sendRequest(method, '/action?param1=str&param2=str2'),
+        'with status 200': function (e, response) {
+            assert.isNull(e);
+            assert.equal(response.statusCode, 200);
+        },
+        'and send params back': {
+            topic: function (response) {
+                var self = this;
+                response
+                    .on('data', function (data) {
+                        self.callback(null, data.toString());
+                    })
+                    .on('error', function (e) {
+                        self.callback(e);
+                    })
+            },
+            'as response body': function (e, data) {
+                assert.equal(data, 'str,str2');
+            }
+        }
+    }
+}
+
 vows.describe('http server').addBatch({
     'simple http connection': {
         'should not fall on a start': function () {
@@ -14,38 +79,13 @@ vows.describe('http server').addBatch({
                 });
             });
         },
-        'should give files from the public folder': {
-            topic: function () {
-                var self = this;
-                http.request({
-                    port: simpleAppPort,
-                    path: '/test.html'
-                }, function (response) {
-                    self.callback(null, response);
-                }).on('error', function (e) {
-                    self.callback(e);
-                }).end();
-            },
-            'with status 200': function (e, response) {
-                assert.isNull(e);
-                assert.equal(response.statusCode, 200);
-            },
-            'with content': {
-                topic: function (response) {
-                    var self = this;
-                    response
-                        .on('data', function (data) {
-                            self.callback(null, data.toString());
-                        })
-                        .on('error', function (e) {
-                            self.callback(e);
-                        })
-                },
-                'equals to Hello World': function (e, data) {
-                    assert.equal(data, 'Hello World');
-
-                }
-            }
+        'should give files from the public folder on': {
+            'GET request': queryFileTest('get'),
+            'POST request': queryFileTest('post')
+        },
+        'should properly process': {
+            'GET request with params': queryWithParams('get'),
+            'POST request with params': queryWithParams('post')
         }
     },
     'http connection with basic auth': {
